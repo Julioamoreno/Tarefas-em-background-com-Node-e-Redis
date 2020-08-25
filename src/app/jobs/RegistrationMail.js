9e6bacacf6b20582a1aae6ae6cc4b2211f929f71
Júlio Armando <julioarmando321@gmail.com>
2020-08-25 00:09:50 -0300

    Commit inicial

---
+++ src/app/jobs/RegistrationMail.js
@@ -0,0 +1,22 @@
+import Mail from '../lib/Mail';
+
+export default {
+	key: 'RegistrationMail',
+	options: {
+		delay: 5000,
+		priority: 3,
+	},
+
+	async handle({ data }) {
+		const { user } = data;
+
+		await Mail.sendMail({
+			from: 'Cadastro <julioarmando321@gmail.com>',
+			to: `${user.name} <${user.email}>`,
+			subject: 'Cadastro de usuário',
+			html: `Olá ${user.name}, seja bem vindo.
+		  Seu usuário foi cadastrado com sucesso.
+		  `,
+		});
+	},
+};
